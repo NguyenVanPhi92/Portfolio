f7991df35165c027a5f96ab3488488438f95325b
nvPhi@02 <nguyenvanphidmx123455@gmail.com>
2023-07-12 00:13:45 +0700

    first commit

---
+++ src/components/About/Toolstack.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Col, Row } from 'react-bootstrap'
+import { CHROME, GIT, GITHUB, TRELLO, VSCODE, WINDOW } from '../../Assets/tech'
+
+function Toolstack() {
+    return (
+        <Row style={{ justifyContent: 'center', paddingBottom: '50px' }}>
+            <Col xs={4} md={2} className='tech-icons'>
+                <img src={WINDOW} alt='' width={80} height={80} />
+            </Col>
+            <Col xs={4} md={2} className='tech-icons'>
+                <img src={VSCODE} alt='' width={80} height={60} />
+            </Col>
+            <Col xs={4} md={2} className='tech-icons'>
+                <img src={TRELLO} alt='' width={90} height={90} />
+            </Col>
+            <Col xs={4} md={2} className='tech-icons'>
+                <img src={CHROME} alt='' width={70} height={70} />
+            </Col>
+            <Col xs={4} md={2} className='tech-icons'>
+                <img src={GITHUB} alt='' width={90} height={90} />
+            </Col>
+            <Col xs={4} md={2} className='tech-icons'>
+                <img src={GIT} alt='' width={90} height={90} />
+            </Col>
+        </Row>
+    )
+}
+
+export default Toolstack
